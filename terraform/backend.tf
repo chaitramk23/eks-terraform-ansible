53f394d9c48634a77cde1ce2f8ae98282d5fae03
chaitramk <cmk6790@gmail.com>
2026-03-13 22:55:39 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eks-terraform-state-bucket-12345"
     key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
