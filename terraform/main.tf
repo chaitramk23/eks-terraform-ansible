0d30ef77591b87c70dfffee0e45569b991927216
chaitramk <cmk6790@gmail.com>
2026-03-13 22:17:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,6 @@
-terraform {
-  required_version = ">=1.3"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>5.0"
-    }
-  }
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
 }
