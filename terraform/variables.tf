75ef105ad79f04284a86ea1773cd90e6a03abafe
chaitramk <cmk6790@gmail.com>
2026-03-13 22:11:24 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  default     = "devops-eks-cluster"
+}
